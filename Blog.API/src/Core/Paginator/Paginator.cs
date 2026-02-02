0989848343eedac28e995ccf64ac7dbe11ff4ec8
Maksym <qlowlight@gmail.com>
2026-02-02 18:01:01 +0200

    add Paginator

---
+++ Blog.API/src/Core/Paginator/Paginator.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.API.Core.Paginator;
+
+public class Paginator<T>(int pageNumber, int pageSize, int totalItems, List<T> items)
+{
+    public int PageNumber { get; set; } = pageNumber;
+    public int PageSize { get; set; } = pageSize;
+    public int TotalItemsCount { get; set; } = totalItems;
+    public List<T> Items { get; set; }  = items;
+}
