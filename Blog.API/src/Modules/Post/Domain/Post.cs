60d486dd2c8443abeb1c2b06601a0f7cca4198f5
Maksym <qlowlight@gmail.com>
2026-02-02 12:13:20 +0200

    add Post entity

---
+++ Blog.API/src/Modules/Post/Domain/Post.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.API.Modules.Post.Domain;
+
+
+public class Post
+{
+    public int Id { get; set; }
+    
+    public string Title { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+    
+    public string Content { get; set; } = string.Empty;
+}
