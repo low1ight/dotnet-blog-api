2e0aa1dc2be860632e9d230ca225288e8a498ce8
Maksym <qlowlight@gmail.com>
2026-02-03 15:36:54 +0200

    remove console.write

--- Blog.API/src/Modules/Post/PostsEndpoints.cs
+++ Blog.API/src/Modules/Post/PostsEndpoints.cs
@@ -6,7 +6,6 @@ using Blog.API.Modules.Post.Controllers.ViewDto;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Dynamic.Core;
 using Blog.API.Core.Dto;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.API.Modules.Post;
 
@@ -14,49 +13,42 @@ public static class PostsEndpoints
 {
     public static RouteGroupBuilder MapPostsEndpoints(this RouteGroupBuilder group)
     {
-
         group.MapGet("/", async (
-            AppDbContext context, 
+            AppDbContext context,
             [AsParameters] BaseQueryParams baseQuery,
             [AsParameters] PostQueryParams postQuery) =>
         {
-            
-            
-            Console.WriteLine("dir: " + baseQuery);
-            Console.WriteLine("dir: " + postQuery);
-            
-            
             var query = context.Posts.AsQueryable();
-            
+
             if (!string.IsNullOrWhiteSpace(postQuery.TitleSearchTerm))
             {
                 query = query.Where(p => p.Title.Contains(postQuery.TitleSearchTerm));
             }
-            
+
             var sortDirection = baseQuery.SortDirection == SortOrder.Asc ? "asc" : "desc";
-            
-            var sortBy = string.IsNullOrWhiteSpace(postQuery.SortBy) 
-                ? "Id" 
+
+            var sortBy = string.IsNullOrWhiteSpace(postQuery.SortBy)
+                ? "Id"
                 : postQuery.SortBy;
 
             query = query.OrderBy($"{sortBy} {sortDirection}");
-            
-            
+
+
             var totalCount = await query.CountAsync();
-            
-            
+
+
             var result = await query
                 .Skip((baseQuery.PageNumber - 1) * baseQuery.PageSize)
                 .Take(baseQuery.PageSize)
                 .Select(post => new PostViewDto
-            
+
                 {
                     Id = post.Id,
                     Title = post.Title,
                     Description = post.Description,
                     Content = post.Content,
                 }).ToListAsync();
-            
+
             return new Paginator<PostViewDto>
             {
                 PageNumber = baseQuery.PageNumber,
@@ -64,15 +56,9 @@ public static class PostsEndpoints
                 TotalItemsCount = totalCount,
                 Items = result
             };
-            
-
-
-
-
         });
-        
-        
-        
+
+
         group.MapGet("/{id:int}", async (AppDbContext context, int id) =>
         {
             var post = await context.Posts
@@ -84,38 +70,31 @@ public static class PostsEndpoints
                     Description = post.Description,
                     Content = post.Content,
                 }).FirstOrDefaultAsync();
-            
+
             return post is not null ? Results.Ok(post) : Results.NotFound();
         });
-        
-        
-        
-        
-        
+
+
         group.MapPost("/", async (PostsService postsService, PostInputDto dto) =>
         {
-             var createdUserId = await postsService.CreatePostAsync(dto);
-             return Results.Created();
-            
+            var createdUserId = await postsService.CreatePostAsync(dto);
+            return Results.Created($"api/posts/{createdUserId}",createdUserId);
         });
-        
-        
+
+
         group.MapPut("/{id:int}", async (PostsService postsService, PostInputDto dto, int id) =>
         {
             bool result = await postsService.UpdatePostAsync(dto, id);
             return result ? Results.NoContent() : Results.NotFound();
-            
         });
-        
-        
+
+
         group.MapDelete("/{id:int}", async (PostsService postsService, int id) =>
         {
             bool result = await postsService.DeletePostAsync(id);
             return result ? Results.NoContent() : Results.NotFound();
-            
         });
-        
-        
+
 
         return group;
     }
